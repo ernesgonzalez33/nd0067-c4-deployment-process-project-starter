31682196fbd51396de108fbb4406af6dbf05c50e
Ernesto González <ernesgonzalez.33.egc@icloud.com>
2023-03-05 01:29:26 +0100

    Fix linting error

--- udagram/udagram-frontend/src/environments/environment.prod.ts
+++ udagram/udagram-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
-import * as dotenv from "dotenv";
+import * as dotenv from 'dotenv';
 dotenv.config();
 // This file can be replaced during build by using the `fileReplacements` array.
 // `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
